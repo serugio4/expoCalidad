e25ef594e190931db4721c9b1e497b92cfe8b50c
sergio garcia <serugio4444@hotmail.com>
2017-10-06 11:50:42 -0500

    version 1.1

--- src/pruebas/PruebasFibonacci.java
+++ src/pruebas/PruebasFibonacci.java
@@ -13,32 +13,7 @@ public class PruebasFibonacci {
 	
 	Fibonacci fi = new Fibonacci();
 
-	@Test
-	public void resultadoTamaño5() {
-		
-		String esperado = "1, 1, 2, 3, 5";
-					
-		assertEquals(esperado, fi.calcularFibonacci(5));
 	
-	}
-	
-	@Test
-	public void resultadoTamaño10() {
-		
-		String esperado = "1, 1, 2, 3, 5, 8, 13, 21, 34, 55";
-					
-		assertEquals(esperado, fi.calcularFibonacci(10));
-	
-	}
-	
-	@Test
-	public void resultadoTamaño15() {
-		
-		String esperado = "1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610";
-					
-		assertEquals(esperado, fi.calcularFibonacci(15));
-	
-	}
 }
 	
 	
